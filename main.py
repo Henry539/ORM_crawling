6b8aa72ae76421c39766c782c32536f4f67a54fe
Henry539 <supa.thanhson@gmail.com>
2022-06-18 03:18:19 +0700

    "03:18 AM 18/06: update!"

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-import time
-
 from func import *
 
-import requests
-
 Base.metadata.create_all(engine)
+import requests
+import csv
 
-def read_shopee(itemid,shopid):
 
-    proxies = {"http": "36.94.40.123:9812"}
+def read_data_txt():
+    proxy_list = []
+    with open('data.txt', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            proxy_list.append(row[0])
+    return proxy_list
 
-    response = requests.get(f"https://shopee.vn/api/v4/item/get?itemid={itemid}&shopid={shopid}", proxies=proxies)
-    data = response.json()
-    return data["data"]["historical_sold"]
+def read_shopee(itemid,shopid,proxy):
+    try:
+        response = requests.get(f"https://shopee.vn/api/v4/item/get?itemid={itemid}&shopid={shopid}", proxies={'http' : proxy,'https': proxy}, timeout=2)
+        data = response.json()
+        return data["data"]["historical_sold"]
+    except:
+        return -1
 
 
 def update_data(itemid, shopid, historical_sold):
@@ -29,13 +36,20 @@ def update_data(itemid, shopid, historical_sold):
 
 list_shope=[[9805929978,120324880],[16474833352,18796445],[18701518960,120324880],[9256605454,317428602],[10412346716,210089851]]
 
+def check(itemid,shopid,num):
+    print(f"{num+1}")
+    if num == 9:
+        return False
+    if read_shopee(itemid, shopid, list_proxy[num]) != -1:
+        his_sold = read_shopee(itemid, shopid, list_proxy[num])
+        update_data(itemid, shopid, his_sold)
+        return True
+    return check(itemid,shopid,num+1)
+
 if __name__ == "__main__":
-    count = 0
-    while True:
-        for i in list_shope:
-            his_sold = read_shopee(i[0],i[1])
-            update_data(i[0],i[1],his_sold)
-        count+=1
-        if count == 2:
-            break
-        time.sleep(20)
+    list_proxy = read_data_txt()
+    for i in list_shope:
+        if check(i[0],i[1],0):
+            continue
+        print("NON-PROXY_IP CAN USE!")
+        break
